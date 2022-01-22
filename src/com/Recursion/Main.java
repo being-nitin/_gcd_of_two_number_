e1ce60719a0328c90d763d0008988924dbfcac10
Nitin <nsinghboss02@gmail.com>
2022-01-22 12:18:00 +0530

    Initial commit

---
+++ src/com/Recursion/Main.java
@@ -0,0 +1,16 @@
+package com.Recursion;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+	// java program to find the gcd of a number that is HCF.
+    int ans = gcd(3,6);
+        System.out.println(ans);
+    }
+    public static int gcd(int a,int b){
+        if(a==0){
+            return b;
+        }
+        return gcd(b%a,a);
+    }
+}
